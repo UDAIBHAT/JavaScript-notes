64a1b34300ebbd183f4424772f97976d047216e6
Your Name <udaibhat181@gmail.com>
2024-06-14 17:59:11 +0530

    update

--- Arith_oprs.js
+++ Arith_oprs.js
@@ -15,8 +15,8 @@
 
 
 
-let value = 3
-let negValue = -value
+// let value = 3
+// let negValue = -value
 // console.log(negValue);
 
 // console.log(2+2);
@@ -30,18 +30,18 @@ let negValue = -value
 // [Incrementing]
 // The increment operator (++) increments numbers.
 
-let x = 5;
-x++;
-console.log(x);
+// let x = 5;
+// x++;
+// console.log(x);
 
 // [Decrementing]
 // The decrement operator (--) decrements numbers.
 
-{
-    let x = 5;
-x--;
-console.log(x);
-}
+// {
+//     let x = 5;
+// x--;
+// console.log(x);
+// }
 
 //                                             [Operator Precedence]
 
@@ -68,3 +68,32 @@ console.log(x);
 // When many operations have the same precedence (like addition and subtraction or multiplication and division), they are computed from left to right:
 
 
+//                            pre & post increment
+
+
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+//https://youtu.be/aYqsb3h6JGQ?si=1N36qq6v99FnvJrB 
+
+            // let x = 5 ;
+            // let y = 5 ;
+
+            // y = ++x ;    it will first increment then assign the value to y .
+
+            // console.log(y);
+            // console.log(x);
+
+            // {
+            //     let x = 5 ;
+            // let y = 5 ;
+
+            // y = x++ ;    it will first assign the value and after that it will increment the value.
+
+            // console.log(y);
+            // console.log(x);
+
+
+
+}
