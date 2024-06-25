1cc2abe2aa310c5e7226a408fb377e1302a7bdf1
UDAI BHAT <udaibhat181@gmail.com>
2024-06-26 00:38:26 +0530

    DAY-6

---
+++ Dates.js
@@ -0,0 +1,36 @@
+const d = new Date();
+console.log(d);
+console.log(d.toString());
+console.log(d.toDateString());
+console.log(d.toUTCString());
+console.log(d.toISOString());
+console.log(d.toLocaleString());
+
+{
+    let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+}
+
+//  There are 9 ways to create a new date object:
+
+// new Date()
+// new Date(date string)
+
+// new Date(year,month)
+// new Date(year,month,day)
+// new Date(year,month,day,hours)
+// new Date(year,month,day,hours,minutes)
+// new Date(year,month,day,hours,minutes,seconds)
+// new Date(year,month,day,hours,minutes,seconds,ms)
+
+// new Date(milliseconds)
+
+
+//  FOR Date Formats:- https://www.w3schools.com/js/js_date_formats.asp
+
+//  FOR GET Date Methods :- https://www.w3schools.com/js/js_date_methods.asp
+
+//   FOR SET Date Methods :-  https://www.w3schools.com/js/js_date_methods_set.asp
